6c04c8448909fe6b53fa1146951d0d75f7f77139
Andrey <andrey.sviontek@gmail.com>
2020-03-09 14:40:07 +0500

    solution: start to decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function toReadable (number) {
-  
+    let numbers = [zero, one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen, eighteen, nineteen];
+    let dozens =[twenty, thirty, forty, fifty, sixty, seventy, eighty, ninety];
+    if (number < 20) {
+        return numbers[number];
+    }
 }
