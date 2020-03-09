c405377c66dc3dff881d998a72f52c0080c090fb
Andrey <andrey.sviontek@gmail.com>
2020-03-09 17:26:15 +0500

    solution: the task is harder than it seems for the first time

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 module.exports = function toReadable (number) {
-    let numbers = [zero, one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen, eighteen, nineteen];
-    let dozens =[twenty, thirty, forty, fifty, sixty, seventy, eighty, ninety];
-    if (number < 20) {
-        return numbers[number];
-    }
+    let ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+	let tens = [ '', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety' ];
+    let firstPart;
+    let remainder;
+    let digitsRemainder;
+    let secondPart;
+    if (number == 0) return 'zero';
+	if (number < 20) {
+		return ones[number];
+	}
+	let digitsNumber = number.toString().split('');
+	if (number >= 20 && number < 100) {
+		return `${tens[digitsNumber[0]]} ${ones[digitsNumber[1]]}`.trim();
+	}
+	if (number >= 100) {
+		firstPart = ones[digitsNumber[0]] + ' hundred';
+		remainder = parseInt(digitsNumber[1] + digitsNumber[2], 10);
+		digitsRemainder = remainder.toString().split('');
+		secondPart = parseInt(digitsNumber[1] + digitsNumber[2]) < 20 ? ones[remainder] : tens[digitsRemainder[0]] + ' ' + ones[digitsRemainder[1]];
+		return `${firstPart} ${secondPart}`.trim();
+	}
 }
